9fb5ecd68d93df5b99d82467995ccbd2c23f5767
Alexey Mulyukin <adiott52@gmail.com>
2024-07-24 21:49:37 +0300

    ДЗ по 3 части почти готово

---
+++ src/main/java/ru/mulyukin/homework/lesson7/Application.java
@@ -0,0 +1,87 @@
+package ru.mulyukin.homework.lesson7;
+
+public class Application {
+    public static void main(String[] args) {
+        int[][] array = {{3, 5, 6, 2}, {0, 0, 0, 0}, {10, -4, 1, 1}, {5, 5, 51, 11}};
+        System.out.println(sumOfPositiveElements(array));
+        print(array);
+        reset(array);
+        print(array);
+        System.out.println(findMax(array));
+        System.out.println(sumSecondLine(array));
+        cube(10);
+
+
+    }
+
+    public static int sumOfPositiveElements(int[][] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] > 0) {
+                    sum += arr[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+
+    public static void cube(int size) {
+        int[][] array = new int[size][size];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (i == 0 || j == 0 || i == size - 1 || j == size - 1) {
+                    array[i][j] = '*';
+                    System.out.print(array[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void reset(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (i == j || i + j == arr.length - 1) {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public static void print(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static int findMax(int[][] array) {
+        int max = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (max < array[i][j]) {
+                    max = array[i][j];
+                }
+            }
+        }
+        return max;
+    }
+
+    public static int sumSecondLine(int[][] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (i == 1) {
+                    sum += array[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+}
